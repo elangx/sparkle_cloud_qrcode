f28c25d412645bb308e7fde727a6eb646be21491
yangxian <yangxian@miliantech.com>
2025-06-30 20:16:18 +0800

    Initial Commit

---
+++ sparkle_cloud_qrcode/sparkle_cloud_qrcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkle_cloud_qrcodeApp.swift
+//  sparkle_cloud_qrcode
+//
+//  Created by Ethan on 2025/6/30.
+//
+
+import SwiftUI
+
+@main
+struct sparkle_cloud_qrcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sparkle_cloud_qrcodeTests/sparkle_cloud_qrcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  sparkle_cloud_qrcodeTests.swift
+//  sparkle_cloud_qrcodeTests
+//
+//  Created by Ethan on 2025/6/30.
+//
+
+import Testing
+@testable import sparkle_cloud_qrcode
+
+struct sparkle_cloud_qrcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
